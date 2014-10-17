92a069b38af41be05dca9912facc780b3d971d4d
derek <derekkshapiro@gmail.com>
2014-10-17 14:56:00 -0700

    Initial commit

---
+++ FTW Group   Product Development & Design_files/animate.js
@@ -0,0 +1,55 @@
+$(document).ready(function() { 
+                $('#increasing1').delay(5000).animate({height:'20px'}, 300, "linear", 
+                function() {$('#increasing2').animate({width: '480px', marginLeft: '0px'}, 1800, "linear",
+                function() {$('#increasing3').animate({height: '82px'}, 500, "linear",
+                function() {$('#increasing4').animate({width: '100px'}, 500, "linear",
+                function() {$('#increasing5').delay(0).animate({width: '370px'}, 1500, "linear",
+                function() {$('#increasing6').delay(0).animate({width: '370px'}, 1500, "linear",
+                function() {$('#increasing7').animate({height: '194px'}, 1000, "linear",
+                function() {$('#increasing8').animate({width: '650px', marginLeft: '0px'}, 2500, "linear",
+                function() {$('#increasing9').animate({height: '110px'}, 500, "linear",
+                function() {$('#increasing10').animate({height: '100px'}, 500, "linear",
+                function() {$('#increasing11').animate({width: '400px'}, 1700, "linear",
+                function() {$('#increasing12').animate({height: '110px'}, 550, "linear",
+                function() {$('#increasing13a').animate({height: '40px'}, 300, "linear",
+                function() {$('#increasing13').animate({height: '100px'}, 500, "linear",
+                function() {$('#increasing14').animate({width: '170px'}, 1000, "linear",
+                function() {$('#increasing15').animate({height: '102px'}, 520, "linear",
+                function() {$('#increasing16').animate({height: '50px'}, 400, "linear",
+                function() {$('#increasing17').animate({width: '395px', marginLeft: '0px'}, 1500, "linear",
+                function() {$('#increasing18').animate({height: '20px'}, 300, "linear",
+                function() {$('#increasing19').animate({width: '75px'}, 700, "linear",
+                function() {$('#increasing20').animate({width: '75px'}, 700, "linear",
+                function() {$('#increasing21').animate({height: '121px'}, 750, "linear",
+                function() {$('#increasing22').animate({width: '85px', marginLeft: '0px'}, 800, "linear",
+                function() {$('#increasing22a').animate({width: '155px', marginLeft: '0px'}, 950, "linear",
+                function() {$('#increasing23').animate({height: '295px'}, 1200, "linear",
+                function() {$('#increasing24').animate({width: '287px'}, 1700, "linear"
+                                );}
+                                );}
+                                );}
+                                );}
+                                );}
+                                );}
+                                );}
+                                );}
+                                );}
+                                );}
+                );}                       
+                        );}
+                        );}
+                        );}
+                        );}
+                        );}
+                        );}
+                  );}
+                  );}
+
+);}
+);}
+);}
+);}
+);}
+);}
+);}
+); 
---
+++ FTW Group   Product Development & Design_files/main.js
@@ -0,0 +1,89 @@
+$(document).ready(function () {
+$(".js-slogan").each(function () {
+		var jSlogan = $(this);
+		var pSet = $("p", jSlogan);
+
+		pSet.filter(":gt(0)").addClass("right").show();
+
+		var activeIndex = 0;
+
+		var transitionDuration = parseInt(parseFloat(pSet.eq(0).find("strong").css("transition-duration")) * 1000, 10);
+		var delay = 5000; // changing slides' delay
+
+		var changeSlide = function () {
+			var pCurrent = pSet.eq(activeIndex);
+			if (activeIndex === pSet.length - 1) {
+				activeIndex = 0;
+			} else {
+				activeIndex++;
+			}
+			var pNext = pSet.eq(activeIndex);
+
+			pCurrent.addClass("left");
+			pNext.removeClass("right");
+
+			setTimeout(function () {
+				pCurrent.removeClass("left").addClass("right");
+			}, transitionDuration);
+
+			changeSlideTimeout = setTimeout(changeSlide, delay);
+		};
+
+		var changeSlideTimeout = setTimeout(changeSlide, delay);
+	});
+
+// photo animation
+
+$(function() {
+    var _intervalId;
+    
+    function fadeInLastImg()
+    {
+        var backImg = $('.pictures img:first');
+        backImg.hide();
+        backImg.remove();
+        $('.pictures' ).append( backImg );
+        backImg.fadeIn();
+    }
+        
+    _intervalId = setInterval( function() {
+        fadeInLastImg();
+    }, 5000 );
+
+});
+
+$(function() {
+    var date = new Date();
+
+    var day = date.getDate();
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear();
+
+    if (month < 10) month = "0" + month;
+    if (day < 10) day = "0" + day;
+
+    var today = year + "-" + month + "-" + day;       
+    $("#theDate").attr("value", today);
+	});
+
+window.addEventListener("scroll",function() { 
+   if(window.scrollY > 150) {
+      $('#tinyNav').removeClass(".noShow").fadeIn();
+   }
+   else {
+      $('#tinyNav').addClass(".noShow").fadeOut('fast');
+   }
+},false);
+
+
+// scroll animation
+
+ $(".linking ul li a[href^='#'],a[href^='#project_top'],a[href^='#portfolio'],a[href^='#top'],a[href^='#team'],a[href^='#services']").on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({ scrollTop: $(this.hash).offset().top }, 300);
+
+        // edit: Opera and IE requires the "html" elm. animated
+    });
+
+
+});
